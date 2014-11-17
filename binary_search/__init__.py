ba81ed45052cbeb84f968964c22fdd5ecd26e332
Jimmy <jimmy@motherapp.com>
2014-11-17 18:41:08 +0800

    revise sphinx file

---
+++ binary_search/__init__.py
@@ -0,0 +1,10 @@
+"""Binary search questions.
+
+.. moduleauthor:: Jimmy Cheung <jimmykobe1171@126.com>
+
+"""
+
+import divide_two_integers
+
+def start():
+    "This starts this module running ..."
